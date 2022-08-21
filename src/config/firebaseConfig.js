134e3c735e2a9278ffe661a42f6265eae15105f7
Mihir Upadhyay <sirmihirmsd07@gmail.com>
2022-08-21 20:13:40 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAL5FO5lokY_O_tcLIfeNCVb197Zt3GjG0",
+    authDomain: "crypto-buddy-84aee.firebaseapp.com",
+    projectId: "crypto-buddy-84aee",
+    storageBucket: "crypto-buddy-84aee.appspot.com",
+    messagingSenderId: "74691841591",
+    appId: "1:74691841591:web:96ec6af91e3ef388a7b9b2",
+};
+
+export default firebaseConfig;
